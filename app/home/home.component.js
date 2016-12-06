26fcb1b4ce209dc3857fe94413786a8fbc2b6622
yanchanglu <513894105@qq.com>
2016-12-06 09:10:01 +0800

    update

--- app/home/home.component.js
+++ app/home/home.component.js
@@ -9,20 +9,60 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var HomeComponent = (function () {
-    function HomeComponent() {
+var digit_1 = require('./digit');
+var ball = (function () {
+    function ball() {
     }
+    return ball;
+}());
+exports.ball = ball;
+var HomeComponent = (function () {
+    function HomeComponent(digit) {
+        this.digit = digit;
+        this.endTime = new Date();
+        this.MARGIN_LEFT = 30;
+        this.MARGIN_TOP = 60;
+        this.RADIUS = 8;
+        this.curShowTimeSeconds = 0;
+        this.balls = [];
+        this.colors = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF444", "#CC000C"];
+    }
+    ;
     HomeComponent.prototype.ngOnInit = function () {
-        (window.gettool())();
+        this.endTime.setTime(this.endTime.getTime() + 3600 * 1000);
+        this.initCanvas();
+    };
+    HomeComponent.prototype.initCanvas = function () {
+        var curShowTimeSeconds;
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+        canvas.height = 700;
+        canvas.width = 1400;
+        curShowTimeSeconds = this.getCurShowTimeSeconds();
+        setInterval(function () {
+            this.render(context);
+            this.update();
+        }, 50);
+    };
+    HomeComponent.prototype.getCurShowTimeSeconds = function () {
+        var curTime = new Date();
+        var ret = this.endTime.getTime() - curTime.getTime();
+        ret = Math.round(ret / 1000);
+        return ret >= 0 ? ret : 0;
+    };
+    HomeComponent.prototype.render = function (cxt) {
+    };
+    HomeComponent.prototype.update = function () {
     };
     HomeComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'my-home',
             templateUrl: './home.html',
-            styleUrls: ['./home.css']
+            styleUrls: ['./home.css'],
+            providers: [digit_1.Digit]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [digit_1.Digit])
     ], HomeComponent);
     return HomeComponent;
 }());
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,12 +1,58 @@
 import { Component,OnInit } from '@angular/core';
+import {Digit} from './digit';
+export class  ball{
+        x:number;
+        y:number;
+        g:number;
+        vx:number;
+        vy:number;
+        color:string;
+    }
 @Component({
   moduleId: module.id,
   selector: 'my-home',
   templateUrl:'./home.html',
-  styleUrls:['./home.css']
+  styleUrls:['./home.css'],
+  providers:[Digit]
 })
 export class HomeComponent  implements OnInit{
+    constructor(private digit:Digit){};
+    endTime:Date = new Date();
+    MARGIN_LEFT = 30;
+    MARGIN_TOP = 60;
+    RADIUS = 8;
+    curShowTimeSeconds = 0;
+   
+    balls:ball[] = [];
+    colors:string[] = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF444", "#CC000C"];
+
     ngOnInit(){
-        ((<any>window).gettool())();
+        this.endTime.setTime(this.endTime.getTime() + 3600 * 1000);
+        this.initCanvas();
+    }
+   
+    initCanvas(){
+        var curShowTimeSeconds:number;
+        var canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        var context = canvas.getContext("2d");
+        canvas.height = 700;
+        canvas.width = 1400;
+        curShowTimeSeconds=this.getCurShowTimeSeconds();
+        setInterval(function() {
+            this.render(context);
+            this.update();
+        }, 50);
+    }
+    getCurShowTimeSeconds():number{
+        var curTime:Date = new Date();
+        var ret =this.endTime.getTime() - curTime.getTime();
+        ret = Math.round(ret / 1000);
+        return ret >= 0 ? ret : 0;
+    }
+    render(cxt:CanvasRenderingContext2D){
+
+    }
+    update(){
+
     }
 }
